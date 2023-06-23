01744ab76a157debe5783ca378a105e615167a17
wangkang <hust.wangkang@gmail.com>
2023-06-23 23:02:14 +0800

    [v0.9] 用opengl画了一个三角形。        这里有个问题，我的windows的opengl是4.6的，这个fragmentShader可以正常执行，将三角形渲染成橙色，但是wsl的opengl是3.1版本的，wsl里面三角形还是白色。

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <SDL.h>
 #include <glad/glad.h>
+#include <vector>
 
 #undef main
 
@@ -14,6 +15,74 @@ SDL_GLContext gOpenGLContext = nullptr;
 
 bool gQuit=false;
 
+//VAO
+GLuint gVertextArrayObject =0;
+
+//VBO
+GLuint gVertextBufferObject=0;
+
+//Program Object (for our shaders)
+GLuint gGraphicsPipelineShaderProgram = 0;
+
+
+const std::string gVertexShaderSource =
+        "#version 410 core\n"
+        "in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(position.x,position.y,position.z,position.w);\n"
+        "}\n";
+
+const std::string gFragmentShaderSource =
+        "#version 410 core\n"
+        "out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "    color = vec4(1.0f,0.5f,0.0f,1.0f);\n"
+        "}\n";
+
+
+GLuint CompileShader(GLuint type, const std::string& source){
+    GLuint shaderObject;
+    if(type == GL_VERTEX_SHADER){
+        shaderObject = glCreateShader(GL_VERTEX_SHADER);
+    }else if (type == GL_FRAGMENT_SHADER) {
+        shaderObject = glCreateShader(GL_FRAGMENT_SHADER);
+    }
+
+    const char* src=source.c_str();
+    glShaderSource(shaderObject,1,&src,nullptr);
+    glCompileShader(shaderObject);
+
+    return shaderObject;
+}
+
+
+GLuint CreateShaderProgram(const std::string& vs,const std::string& fs)
+{
+    GLuint programObject = glCreateProgram();
+    GLuint myVertexShader = CompileShader(GL_VERTEX_SHADER,vs);
+    GLuint myFragmentShader =CompileShader(GL_FRAGMENT_SHADER,fs);
+
+    glAttachShader(programObject,myVertexShader);
+    glAttachShader(programObject,myFragmentShader);
+    glLinkProgram(programObject);
+
+    //validate our program
+    glValidateProgram(programObject);
+
+    //glDetachShader,glDeleteShader
+
+    return programObject;
+
+}
+
+void CreateGraphicsPipeline()
+{
+    gGraphicsPipelineShaderProgram = CreateShaderProgram(gVertexShaderSource,gFragmentShaderSource);
+}
+
+
 
 void GetOpenGLVersionInfo()
 {
@@ -22,6 +91,37 @@ void GetOpenGLVersionInfo()
     std::cout<<"version: "<<glGetString(GL_VERSION)<<std::endl;
     std::cout<<"Shading Language: "<<glGetString(GL_SHADING_LANGUAGE_VERSION)<<std::endl;
 }
+
+
+void VertexSpecification()
+{
+    //Lives on the cpu
+    const std::vector<GLfloat> vertextPosition{
+        // x       y      z
+          -0.8f, -0.8f, 0.0f,  //vertex 1
+           0.8f, -0.8f, 0.0f,  //vertex 2
+           0.0f, 0.8f, 0.0f    //vertex 3
+    };
+
+    //We start setting things up
+    //on the GPU
+    glGenVertexArrays(1,&gVertextArrayObject);
+    glBindVertexArray(gVertextArrayObject);
+
+    //start generating our VBO
+    glGenBuffers(1,&gVertextBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, gVertextBufferObject);
+    glBufferData(GL_ARRAY_BUFFER,vertextPosition.size()*sizeof(GLfloat),vertextPosition.data(),GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
+
+    //clean up
+    glBindVertexArray(0);
+    glDisableVertexAttribArray(0);
+
+}
+
 
 
 void InitializeProgram()
@@ -81,11 +181,22 @@ void Input()
 }
 void PreDraw()
 {
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_CULL_FACE);
 
+    glViewport(0,0,gScreenWidth,gScreenHeight);
+    glClearColor(1.f,1.f,0.1f,1.f);
+
+    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(gGraphicsPipelineShaderProgram);
 }
 void Draw()
 {
+    glBindVertexArray(gVertextArrayObject);
+    glBindBuffer(GL_ARRAY_BUFFER,gVertextBufferObject);
 
+    glDrawArrays(GL_TRIANGLES,0,3);
 }
 
 
@@ -114,7 +225,12 @@ void CleanUp()
 int main() {
     InitializeProgram();
 
+    VertexSpecification();
+
+    CreateGraphicsPipeline();
+
     MainLoop();
+
 
     CleanUp();
 
